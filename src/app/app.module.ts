f9c01003e85f4d094703d8cd438f8b08045364c2
patrikx3 <alabard@gmail.com>
2020-04-29 22:58:52 +0200

    robo8x [chore]   4/29/2020, 10:58:52 PM

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { AngularHttpCacheInterceptorModule } from '../../projects/angular-http-cache-interceptor/src/lib/angular-http-cache-interceptor.module'
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularHttpCacheInterceptorModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
