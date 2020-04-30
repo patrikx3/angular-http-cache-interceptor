3628f82bb303e5ddad88004d293b9b5989aabc70
patrikx3 <alabard@gmail.com>
2020-04-30 18:55:30 +0200

    robo8x [chore]   4/30/2020, 6:55:30 PM

--- scripts/post-lib-build.js
+++ scripts/post-lib-build.js
@@ -2,30 +2,31 @@
 const fs = require('fs').promises
 
 const execAsync = async () => {
-  const rootPkgName = require('../package.json')
+  const rootPkg = require('../package.json')
   const pkgName = __dirname + '/../release/angular-http-cache-interceptor/package.json'
 
   let pkg = require(pkgName)
   pkg.devDependencies = pkg.devDependencies || {}
-  pkg.name = rootPkgName.name
-  pkg.devDependencies['corifeus-builder'] = rootPkgName.devDependencies['corifeus-builder']
+  pkg.name = rootPkg.name
+  pkg.devDependencies['corifeus-builder'] = rootPkg.devDependencies['corifeus-builder']
   pkg.corifeus = {
     publish: true
   }
 
-  delete rootPkgName.devDependencies
-  delete rootPkgName.dependencies
-  delete rootPkgName.corifeus
-  delete rootPkgName.scripts
-  delete rootPkgName.private
-  pkg = Object.assign(pkg, rootPkgName)
+  delete rootPkg.devDependencies
+  delete rootPkg.dependencies
+  delete rootPkg.corifeus
+  delete rootPkg.scripts
+  delete rootPkg.private
+  pkg = Object.assign(pkg, rootPkg)
   const data = JSON.stringify(pkg, null, 4)
   await fs.writeFile(pkgName, data)
 
-  const finalRootPkgName = JSON.parse((await fs.readFile('../package.json')).toString())
-  finalRootPkgName.dependencies['p3x-interceptor'] = `npm:p3x-angular-http-cache-interceptor@^${finalRootPkgName.version}`
+  const finalRootPkg = JSON.parse((await fs.readFile('../package.json')).toString())
+  finalRootPkg.dependencies['p3x-interceptor'] = `npm:p3x-angular-http-cache-interceptor@^${finalRootPkg.version}`
 
-  const finalRootPkgNameData = JSON.stringify(finalRootPkgName, null, 4)
+  const finalRootPkgNameData = JSON.stringify(finalRootPkg, null, 4)
+  console.log(finalRootPkgNameData)
   await fs.writeFile('../package.json', finalRootPkgNameData)
 
 }
