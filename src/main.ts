4ae85e143e64269172e6f0590f57c8f7f38c0c3e
patrikx3 <alabard@gmail.com>
2020-06-20 05:59:54 +0200

    robo8x [chore]   6/20/2020, 5:59:54 AM

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,15 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+window['corifeus'] = {
+  core: {
+    http: {
+      status: 200,
+      counter: 0,
+    }
+  }
+}
+
 if (environment.production) {
   enableProdMode();
 }
